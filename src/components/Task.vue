9785703fc7ebd0cb4166bd1b99b4c16e5b083f31
Jorne <mr.jorne@gmail.com>
2021-04-21 21:20:22 +0200

    setup and Task.vue
    setup; project, router, home.vue and created task component.

---
+++ src/components/Task.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="task">
+    <div class="actions" @click="toggleDetails">
+      <h3>{{ task.title }}</h3>
+    </div>
+    <div v-if="showDetails" class="details">
+      <p>{{ task.details }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["task"],
+  data(){
+      return{
+          showDetails: false
+      }
+  },
+  methods:{
+      toggleDetails(){
+          this.showDetails = !this.showDetails;
+      }
+  }
+}
+</script>
+
+<style>
+.task{
+    margin: 2rem auto;
+    background: white;
+    padding: 1rem 2rem;
+    border-radius: 0.5rem;
+    box-shadow: 0.4rem 0.8rem 1.2rem rgba(0,0,0,0.05);
+    border-left: 0.5rem solid #5c90dd;
+}
+.actions h3{
+    cursor: pointer;
+}
+
+.task {
+	background: linear-gradient(-45deg, #e9724e21, #e73c7e0e, #23a5d51c, #23d5ab36);
+	background-size: 400% 400%;
+	animation: gradient 15s ease infinite;
+}
+
+@keyframes gradient {
+	0% {
+		background-position: 0% 50%;
+	}
+	50% {
+		background-position: 100% 50%;
+	}
+	100% {
+		background-position: 0% 50%;
+	}
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <div v-if="tasks.length">
+      <div v-for="task in tasks" :key="task.id">
+        <Task :task="task" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "@/components/Task.vue";
+
+export default {
+  name: "Home",
+  components: { Task },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/tasks")
+      .then((res) => res.json())
+      .then((data) => (this.tasks = data))
+      .catch((err) =>
+        console.log(
+          "something went wrong while reading the json file: " + err.message
+        )
+      );
+  },
+};
+</script>
+<style>
+.home{
+  background-image: url'@/assets/images/canopy-by-jorne.jpg';
+}
+</style>
